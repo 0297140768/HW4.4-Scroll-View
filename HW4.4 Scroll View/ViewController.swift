6288a0a170c24be8b45b0a65ba1151bd6aa42c3b
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-07 11:51:04 +0300

    Ready

--- HW4.4 Scroll View/ViewController.swift
+++ HW4.4 Scroll View/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        imageView = UIImageView(image: UIImage(named: "image"))
+        imageView.frame = CGRect(x: 0, y: 0, width: imageView.bounds.width*(UIScreen.main.bounds.height/imageView.bounds.height), height: UIScreen.main.bounds.height)
 
+        let scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.addSubview(imageView)
+        scrollView.minimumZoomScale = 1
+        scrollView.maximumZoomScale = 4.0
+        scrollView.delegate = self
+        view.addSubview(scrollView)
+        scrollView.scrollRectToVisible(CGRect(x: imageView.bounds.width/2 - UIScreen.main.bounds.width/2, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), animated: false)
+        
+    }
+    
+    
+
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 
 }
 
